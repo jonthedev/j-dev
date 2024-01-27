62018c2319d6976f98e57da9fbafcf9e76297359
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-01-27 20:38:10 +0100

    feat: create PhoneNumber component for rendering phone numbers
    refactor: phone numbers to use PhoneNumber component in Contact

--- src/components/ui/Contact.tsx
+++ src/components/ui/Contact.tsx
@@ -2,6 +2,7 @@ import { FaCopyright } from "react-icons/fa"
 import SectionTitle from "@components/Typography/Title/SectionTitle"
 import Link from "@components/Common/Link"
 import Icon from "@components/Icons/Icon"
+import PhoneNumber from "@components/ui/PhoneNumber"
 
 const Contact = () => {
   return (
@@ -9,12 +10,8 @@ const Contact = () => {
       <section className="align-element pb-5 pt-20" id="contact">
         <SectionTitle title="Contact" />
         <div className="mt-4 flex flex-col items-start">
-          <span className="text-sm tracking-wide text-slate-50 md:text-xl">
-            🇬🇧 +447729268750
-          </span>
-          <span className="text-sm tracking-wide text-slate-50 md:text-xl">
-            🇳🇱 +31645170103
-          </span>
+          <PhoneNumber countryFlag="🇬🇧" phoneNumber="447729268750" />
+          <PhoneNumber countryFlag="🇳🇱" phoneNumber="31645170103" />
           <Link
             href="https://www.linkedin.com/in/jonathan-kaonga-5a04871b5/"
             target="_blank"
@@ -22,7 +19,7 @@ const Contact = () => {
             className="link-with-svg"
           >
             <Icon
-              className="h-4 w-4 text-blue-500 md:mt-2 md:h-6 md:w-6"
+              className="h-4 w-4 text-blue-500 md:h-6 md:w-6"
               variant="base"
               name="linkedIn"
             />
