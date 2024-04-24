90b25aaa62f13f8e7a4b525e8a9185b75dd5e9a3
Jonathan D Kaonga <jonthedev0@gmail.com>
2024-04-24 07:56:56 +0200

    fix: move analytics setup to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,14 @@ import About from "@components/ui/About"
 import Clients from "@components/ui/Clients"
 import Projects from "@components/ui/Projects"
 import Contact from "@components/ui/Contact"
+import ReactGA from "react-ga4"
+
+ReactGA.initialize(`${import.meta.env.APP_GOOGLE_ANALYTICS_ID}`)
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+  title: "Hit homepage",
+})
 
 function App() {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,6 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
-import ReactGA from "react-ga4"
-
-ReactGA.initialize(`${import.meta.env.APP_GOOGLE_ANALYTICS_ID}`)
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-  title: "Hit homepage",
-})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
