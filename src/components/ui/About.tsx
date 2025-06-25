240acebcf90b20ca4b40d6a5e7f4bde04bddb048
Jonathan D Kaonga <jonthedev0@gmail.com>
2025-06-26 00:27:22 +0200

    chore: remove image from about me sections feat: update about me text sections

--- src/components/ui/About.tsx
+++ src/components/ui/About.tsx
@@ -1,4 +1,3 @@
-import J2 from "@assets/J2.png"
 import SectionTitle from "@components/Typography/Title/SectionTitle"
 
 import Text from "@components/Typography/Text/Text"
@@ -9,55 +8,43 @@ import Spacer from "@components/Common/Spacer"
 const About = () => {
   return (
     <Section id="about">
-      <div className="grid items-center gap-16 text-lg md:grid-cols-2 md:text-2xl">
-        <img src={J2} alt="Jonathan looking at a mountain" />
-        <article>
-          <SectionTitle title="About" />
-          <Spacer size="large" />
-          <Text size="medium">
-            <Icon name="brain" variant="base" className=" text-pink-500" />
-            <br />I am fueled by the dynamic nature of web development and the
-            boundless possibilities it offers.
-          </Text>
-          <Text size="medium">
-            I thrive on collaborating with like-minded professionals to
-            brainstorm innovative features and discover robust solutions.
-          </Text>
-          <Text size="medium">
-            <Icon name="cog-wheel" variant="base" className=" text-blue-300" />
-            <br />I specialize in front-end development, crafting pixel-perfect
-            UI's that elevate user experiences.
-          </Text>
-          <Text size="medium">
-            <Icon name="trophy" variant="base" className=" text-yellow-500" />
-            <br />
-            Won website of the year award for ANWB's multi-million user web app
-            Traffic Verkeer.
-          </Text>
-          <Text size="medium">
-            <Icon
-              name="reg-address-card"
-              variant="base"
-              className=" text-green-500"
-            />
-            <br />
-            Legal right to work within the United Kingdom and The Netherlands.
-            Opportunities beyond these regions will need a visa sponsorship or
-            be 100% remote.
-          </Text>
-          <Text size="medium">
-            <Icon
-              name="person-rays"
-              variant="base"
-              className="text-amber-700"
-            />
-            <br />
-            Beyond coding, I'm a dance and travel enthusiast. Exploring diverse
-            cultures fuels my creativity, enriching my approach to web
-            development.
-          </Text>
-        </article>
-      </div>
+      <article className="text-lg md:text-2xl">
+        <SectionTitle title="About" />
+        <Spacer size="large" />
+        <Text size="medium">
+          <Icon name="brain" variant="base" className=" text-pink-500" />
+          <br />
+          I'm a front-end developer fueled by the dynamic nature of web
+          development and the boundless possibilities it offers. I thrive in
+          collaborative environments, where brainstorming innovative features
+          and finding robust, scalable solutions is the norm.
+        </Text>
+
+        <Text size="medium">
+          <Icon name="cog-wheel" variant="base" className=" text-blue-300" />
+          <br />I specialize in crafting pixel-perfect UIs that elevate user
+          experiences. My work has been recognized with a Website of the Year
+          award for ANWB's multi-million-user web app, Traffic Verkeer.
+        </Text>
+
+        <Text size="medium">
+          <Icon
+            name="reg-address-card"
+            variant="base"
+            className=" text-green-500"
+          />
+          <br />I hold the legal right to work in both the UK and the
+          Netherlands. For opportunities beyond these regions, I'm open to roles
+          that offer visa sponsorship or are 100% remote.
+        </Text>
+        <Text size="medium">
+          <Icon name="person-rays" variant="base" className="text-amber-700" />
+          <br />
+          Outside of development, I'm a dance and travel enthusiast. Exploring
+          diverse cultures fuels my creativity and adds a fresh perspective to
+          my approach to building digital experiences.
+        </Text>
+      </article>
     </Section>
   )
 }
